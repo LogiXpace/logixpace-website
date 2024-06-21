4825a7f31e76503e9fac56ee56b629723754aadd
MrGeniusProgrammer <91416586+MrGeniusProgrammer@users.noreply.github.com>
2024-06-21 08:55:39 +0000

    formatted with prettier

--- src/lib/helpers/shape.ts
+++ src/lib/helpers/shape.ts
@@ -1,32 +1,34 @@
-import { Vector2D } from "./vector2d";
+import { Vector2D } from './vector2d';
 
+export function calculateBoxFromTwoPoint(
+	start: Vector2D,
+	end: Vector2D
+): { position: Vector2D; width: number; height: number } {
+	const diff = end.clone().subVector(start);
 
-export function calculateBoxFromTwoPoint(start: Vector2D, end: Vector2D): { position: Vector2D, width: number, height: number } {
-  const diff = end.clone().subVector(start);
+	let position = new Vector2D();
+	let width = Math.abs(diff.x);
+	let height = Math.abs(diff.y);
 
-  let position = new Vector2D();
-  let width = Math.abs(diff.x);
-  let height = Math.abs(diff.y);
+	if (diff.x > 0) {
+		if (diff.y > 0) {
+			position.copy(start);
+		} else {
+			position.copy(start);
+			position.y -= height;
+		}
+	} else {
+		if (diff.y > 0) {
+			position.copy(start);
+			position.x -= width;
+		} else {
+			position.copy(end);
+		}
+	}
 
-  if (diff.x > 0) {
-    if (diff.y > 0) {
-      position.copy(start);
-    } else {
-      position.copy(start);
-      position.y -= height;
-    }
-  } else {
-    if (diff.y > 0) {
-      position.copy(start);
-      position.x -= width;
-    } else {
-      position.copy(end);
-    }
-  }
-
-  return {
-    position,
-    width,
-    height
-  }
+	return {
+		position,
+		width,
+		height
+	};
 }
