1dcc1261bc94fff049572952dde5cb7184669747
MrGeniusProgrammer <91416586+MrGeniusProgrammer@users.noreply.github.com>
2024-06-21 08:16:19 +0000

    created a helper function to infer the box shape from two points

---
+++ src/lib/helpers/shape.ts
@@ -0,0 +1,32 @@
+import { Vector2D } from "./vector2d";
+
+
+export function calculateBoxFromTwoPoint(start: Vector2D, end: Vector2D): { position: Vector2D, width: number, height: number } {
+  const diff = end.clone().subVector(start);
+
+  let position = new Vector2D();
+  let width = Math.abs(diff.x);
+  let height = Math.abs(diff.y);
+
+  if (diff.x > 0) {
+    if (diff.y > 0) {
+      position.copy(start);
+    } else {
+      position.copy(start);
+      position.y -= height;
+    }
+  } else {
+    if (diff.y > 0) {
+      position.copy(start);
+      position.x -= width;
+    } else {
+      position.copy(end);
+    }
+  }
+
+  return {
+    position,
+    width,
+    height
+  }
+}
