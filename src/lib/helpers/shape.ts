4ba61191b2a5e67ed3e05442bffea725b02fa66d
MrGeniusProgrammer <91416586+MrGeniusProgrammer@users.noreply.github.com>
2024-06-24 22:32:26 +0000

    added two simple functions: one calculates the position of a rect given its center position and its dimension, the other calculates the closest point, within the line, to a line segment given a point

--- src/lib/helpers/shape.ts
+++ src/lib/helpers/shape.ts
@@ -1,9 +1,13 @@
+import { clamp } from './math';
 import { Vector2D } from './vector2d';
 
-export function calculateBoxFromTwoPoint(
-	start: Vector2D,
-	end: Vector2D
-): { position: Vector2D; width: number; height: number } {
+interface Box {
+	position: Vector2D;
+	width: number;
+	height: number;
+}
+
+export function calculateBoxFromTwoPoint(start: Vector2D, end: Vector2D): Box {
 	const diff = end.clone().subVector(start);
 
 	let position = new Vector2D();
@@ -31,4 +35,29 @@ export function calculateBoxFromTwoPoint(
 		width,
 		height
 	};
+}
+
+export function calculateBoxTopLeftPositionFromCenterPosition(
+	centerPosition: Vector2D,
+	width: number,
+	height: number
+): Vector2D {
+	const position = centerPosition.clone();
+	position.x -= width / 2;
+	position.y -= height / 2;
+
+	return position;
+}
+
+export function getClosestPoint(point: Vector2D, start: Vector2D, end: Vector2D, thickness: number) {
+	const v = end.clone().subVector(start);
+	const w = point.clone().subVector(start);
+
+	const dotProduct = w.dot(v);
+	const vDotv = v.dot(v);
+
+	const t = clamp(dotProduct / vDotv, 0, thickness);
+	const closestPoint = start.clone().addVector(v.multScalar(t));
+
+	return closestPoint;
 }
