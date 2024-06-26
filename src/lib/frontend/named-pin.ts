4dce57bf2cbb8b0bba30cffba326cad640858fc1
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-06-26 20:37:22 +0300

    made with generic pin id type and created a constructor object to create the instance

--- src/lib/frontend/named-pin.ts
+++ src/lib/frontend/named-pin.ts
@@ -1,11 +1,17 @@
 import type { PowerState } from './state';
 
-export class NamedPin {
-	id: number;
+export interface NamedPinProps<T> {
+	id: T;
+	name: string;
+	powerState: PowerState;
+}
+
+export class NamedPin<T> {
+	id: T;
 	name: string;
 	powerState: PowerState;
 
-	constructor(id: number, name: string, powerState: PowerState) {
+	constructor({ id, name, powerState }: NamedPinProps<T>) {
 		this.id = id;
 		this.name = name;
 		this.powerState = powerState;
