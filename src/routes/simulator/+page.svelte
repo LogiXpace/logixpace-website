8513e9c0b39aea0684e5cfee5971c165f7ef241e
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-06-27 07:13:05 +0300

    added a context menu to add chips

--- src/routes/simulator/+page.svelte
+++ src/routes/simulator/+page.svelte
@@ -9,32 +9,28 @@
 	import { Vector2D } from '$lib/helpers/vector2d';
 
 	let simulationContext: SimulationContext<number> | undefined = $state(undefined);
+	let isContextMenuOpen = $state(false);
+	let contextMenuPosition = $state(new Vector2D());
 
 	$effect(() => {
 		if (simulationContext === undefined) {
 			return;
 		}
 
-		simulationContext.addChip({
+		simulationContext.addChip('and', ['input-pin-1', 'input-pin-2'], ['output-pin'], {
 			position: new Vector2D(400, 200),
-			name: 'chip',
-			color: new RGB(0, 0, 0),
-			inputPins: [
-				simulationContext.addChipPin({
-					namedPin: simulationContext.addNamedPin({
-						name: 'input-pin',
-						powerState: 1
-					})
-				})
-			],
-			outputPins: [
-				simulationContext.addChipPin({
-					namedPin: simulationContext.addNamedPin({
-						name: 'output-pin',
-						powerState: 1
-					})
-				})
-			]
+			name: 'and',
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addChip('or', ['input-pin-1', 'input-pin-2'], ['output-pin'], {
+			position: new Vector2D(400, 400),
+			name: 'or',
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addChip('not', ['input-pin'], ['output-pin'], {
+			position: new Vector2D(400, 600),
+			name: 'not',
+			color: new RGB(0, 0, 0)
 		});
 
 		simulationContext.addIO({
@@ -46,9 +42,79 @@
 			}),
 			color: new RGB(0, 0, 0)
 		});
+		simulationContext.addIO({
+			direction: DIRECTION.RIGHT,
+			position: new Vector2D(0, 100),
+			namedPin: simulationContext.addNamedPin({
+				name: 'hello',
+				powerState: POWER_STATE_HIGH
+			}),
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addIO({
+			direction: DIRECTION.RIGHT,
+			position: new Vector2D(0, 200),
+			namedPin: simulationContext.addNamedPin({
+				name: 'hello',
+				powerState: POWER_STATE_HIGH
+			}),
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addIO({
+			direction: DIRECTION.RIGHT,
+			position: new Vector2D(0, 300),
+			namedPin: simulationContext.addNamedPin({
+				name: 'hello',
+				powerState: POWER_STATE_HIGH
+			}),
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addIO({
+			direction: DIRECTION.LEFT,
+			position: new Vector2D(600, 0),
+			namedPin: simulationContext.addNamedPin({
+				name: 'hello',
+				powerState: POWER_STATE_HIGH
+			}),
+			color: new RGB(0, 0, 0)
+		});
+		simulationContext.addIO({
+			direction: DIRECTION.LEFT,
+			position: new Vector2D(600, 100),
+			namedPin: simulationContext.addNamedPin({
+				name: 'hello',
+				powerState: POWER_STATE_HIGH
+			}),
+			color: new RGB(0, 0, 0)
+		});
 	});
+
+	$inspect(simulationContext);
 </script>
 
 <div class="h-screen w-screen overflow-hidden">
-	<Simulation scale={1} adapter={new BackendAdapter()} bind:simulationContext />
+	<Simulation
+		scale={1}
+		adapter={new BackendAdapter()}
+		onVoidContextMenuOpen={(position) => {
+			contextMenuPosition = position;
+			isContextMenuOpen = true;
+		}}
+		bind:simulationContext
+	/>
 </div>
+
+{#if simulationContext !== undefined}
+	<div
+		class="fixed z-10 flex min-w-56 flex-col overflow-hidden rounded-md bg-gray-500 p-1 shadow-lg data-[open=false]:hidden"
+		data-open={isContextMenuOpen ? 'true' : 'false'}
+		style:top="{contextMenuPosition.y}px"
+		style:left="{contextMenuPosition.x}px"
+		onblur={() => (isContextMenuOpen = false)}
+	>
+		<div class="flex items-center text-lg font-bold text-white">Add Chip</div>
+		<div class="flex h-full w-full items-center rounded-sm bg-gray-700 px-2 py-1 text-white">
+			And
+		</div>
+	</div>
+{/if}
