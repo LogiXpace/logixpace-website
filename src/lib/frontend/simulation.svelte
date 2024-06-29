ad048d8b5eb58338719b8b6901dac6566bbca0d2
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-06-29 17:15:19 +0300

    added a simple guide to use the canvas

--- src/lib/frontend/simulation.svelte
+++ src/lib/frontend/simulation.svelte
@@ -76,7 +76,21 @@
 
 <div class="h-full w-full" bind:this={simulationElement}>
 	<canvas bind:this={canvas}></canvas>
-	<div class="fixed left-0 top-0 m-2 text-sm font-semibold">
-		FPS: {FPS.toPrecision(3)}
+	<div
+		class="fixed left-0 top-0 m-2 flex flex-col space-y-2 *:max-w-[20ch] *:text-xs *:font-semibold"
+	>
+		<span>FPS: {FPS.toPrecision(3)}</span>
+		<p>
+			to toggle the inputs, press <kbd>shift</kbd> + mouse click
+		</p>
+		<p>to create new enitities, right click on an empty area</p>
+		<p>
+			to create wires, click on the outlet of any inputs, outputs, or chip pins. then a wire will be
+			held on the mouse cursor until you click on another outlet.
+		</p>
+		<p>
+			finally: have fun creating your own circuits! (but be careful, the simulation lacks so many
+			features and has many bugs)
+		</p>
 	</div>
 </div>
