cb64a3e4be60e116d914ce58c1d7446f8a2803e1
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-06-30 23:24:14 +0300

    added the input and output methods

--- src/lib/frontend/simulation-entity-manager.ts
+++ src/lib/frontend/simulation-entity-manager.ts
@@ -9,6 +9,8 @@ import { Vector2D } from "$lib/helpers/vector2d";
 import type { Collider, PointCollider } from "$lib/helpers/colliders";
 import type { Adapter } from "./adapter";
 import { POWER_STATE_HIGH } from "./state";
+import type { Input } from "./input";
+import type { Output } from "./output";
 
 export type Entity<T> = IO<T> | WirePoint<T> | Chip<T> | ChipPin<T>;
 
@@ -20,8 +22,11 @@ export interface SimulationEntityManagerProps<T> {
 }
 
 export class SimulationEntityManager<T> {
-  private ioTree: QuadTree<IO<T>>;
-  ioQueries = new Set<IO<T>>();
+  private inputTree: QuadTree<Input<T>>;
+  inputQueries = new Set<Input<T>>();
+
+  private outputTree: QuadTree<Output<T>>;
+  outputQueries = new Set<Output<T>>();
 
   private wirePointTree: QuadTree<WirePoint<T>>;
   wirePointQueries = new Set<WirePoint<T>>();
@@ -46,15 +51,20 @@ export class SimulationEntityManager<T> {
     this.adapter = adapter;
 
     const position = new Vector2D(-size / 2, -size / 2);
-    this.ioTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
+    this.inputTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
+    this.outputTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
     this.chipPinTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
     this.chipTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
     this.wirePointTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
     this.wireTree = new QuadTree(maxLevel, maxCapacity, position, size, size);
   }
 
-  insertIO(io: IO<T>) {
-    this.ioTree.insert(io, io.bound);
+  insertInput(input: Input<T>) {
+    this.inputTree.insert(input, input.bound);
+  }
+
+  insertOutput(output: Output<T>) {
+    this.outputTree.insert(output, output.bound);
   }
 
   insertChipPin(chipPin: ChipPin<T>) {
@@ -62,19 +72,23 @@ export class SimulationEntityManager<T> {
   }
 
   insertChip(chip: Chip<T>) {
-    this.chipTree.insert(chip, chip.collider);
+    this.chipTree.insert(chip, chip.bound);
   }
 
   insertWirePoint(wirePoint: WirePoint<T>) {
-    this.wirePointTree.insert(wirePoint, wirePoint.collider);
+    this.wirePointTree.insert(wirePoint, wirePoint.bound);
   }
 
   insertWire(wire: Wire<T>) {
     this.wireTree.insert(wire, wire.collider);
   }
 
-  removeIO(io: IO<T>) {
-    this.ioTree.remove(io, io.collider);
+  removeInput(input: Input<T>) {
+    this.inputTree.remove(input, input.bound);
+  }
+
+  removeOutput(output: Output<T>) {
+    this.outputTree.remove(output, output.bound);
   }
 
   removeChipPin(chipPin: ChipPin<T>) {
@@ -82,19 +96,23 @@ export class SimulationEntityManager<T> {
   }
 
   removeChip(chip: Chip<T>) {
-    this.chipTree.remove(chip, chip.collider);
+    this.chipTree.remove(chip, chip.bound);
   }
 
   removeWirePoint(wirePoint: WirePoint<T>) {
-    this.wirePointTree.remove(wirePoint, wirePoint.collider);
+    this.wirePointTree.remove(wirePoint, wirePoint.bound);
   }
 
   removeWire(wire: Wire<T>) {
     this.wireTree.remove(wire, wire.collider);
   }
 
-  queryIO(collider: Collider) {
-    return this.ioTree.query(collider);
+  queryInput(collider: Collider) {
+    return this.inputTree.query(collider);
+  }
+
+  queryOutput(collider: Collider) {
+    return this.outputTree.query(collider);
   }
 
   queryChipPin(collider: Collider) {
@@ -109,8 +127,16 @@ export class SimulationEntityManager<T> {
     return this.wireTree.query(collider);
   }
 
-  queryIOByPoint(collider: PointCollider) {
-    return this.ioTree.queryByPoint(collider);
+  queryWirePoint(collider: Collider) {
+    return this.wirePointTree.query(collider);
+  }
+
+  queryInputByPoint(collider: PointCollider) {
+    return this.inputTree.queryByPoint(collider);
+  }
+
+  queryOutputByPoint(collider: PointCollider) {
+    return this.outputTree.queryByPoint(collider);
   }
 
   queryWirePointByPoint(collider: PointCollider) {
@@ -139,23 +165,36 @@ export class SimulationEntityManager<T> {
   }
 
   destroyWirePoint(wirePoint: WirePoint<T>) {
-    this.adapter.destroyPin(wirePoint.pinId);
     this.removeWirePoint(wirePoint);
     this.wirePointQueries.delete(wirePoint);
 
     for (const wire of wirePoint.wires) {
       this.destroyWire(wire);
     }
+
+    this.adapter.destroyPin(wirePoint.pinId);
   }
 
-  destroyIO(io: IO<T>) {
-    this.adapter.destroyPin(io.pinId);
-    this.removeIO(io);
-    this.ioQueries.delete(io);
+  destroyInput(input: Input<T>) {
+    this.removeInput(input);
+    this.inputQueries.delete(input);
 
-    for (const wire of io.wires) {
+    for (const wire of input.wires) {
       this.destroyWire(wire);
     }
+
+    this.adapter.destroyPin(input.pinId);
+  }
+
+  destroyOutput(output: Output<T>) {
+    this.removeOutput(output);
+    this.outputQueries.delete(output);
+
+    for (const wire of output.wires) {
+      this.destroyWire(wire);
+    }
+
+    this.adapter.destroyPin(output.pinId);
   }
 
   destroyChipPin(chipPin: ChipPin<T>) {
@@ -165,6 +204,8 @@ export class SimulationEntityManager<T> {
     for (const wire of chipPin.wires) {
       this.destroyWire(wire);
     }
+
+    this.adapter.destroyPin(chipPin.pinId);
   }
 
   destroyChip(chip: Chip<T>) {
@@ -178,20 +219,22 @@ export class SimulationEntityManager<T> {
     for (const outputPin of chip.outputPins) {
       this.destroyChipPin(outputPin);
     }
+
+    this.adapter.destroyChip(chip.id, [], []);
   }
 
   query(collider: Collider) {
-    this.ioQueries = this.ioTree.query(collider);
+    this.inputQueries = this.inputTree.query(collider);
+    this.outputQueries = this.outputTree.query(collider);
     this.chipPinQueries = this.chipPinTree.query(collider);
     this.chipQueries = this.chipTree.query(collider);
     this.wirePointQueries = this.wirePointTree.query(collider);
     this.wireQueries = this.wireTree.query(collider);
-
-    this.debug();
   }
 
   cleanup() {
-    this.ioTree.cleanup();
+    this.inputTree.cleanup();
+    this.outputTree.cleanup();
     this.chipPinTree.cleanup();
     this.chipTree.cleanup();
     this.wirePointTree.cleanup();
@@ -199,7 +242,8 @@ export class SimulationEntityManager<T> {
   }
 
   debug() {
-    console.log('ioQueries.size', this.ioQueries.size);
+    console.log('inputQueries.size', this.inputQueries.size);
+    console.log('outputQueries.size', this.outputQueries.size);
     console.log('chipPinQueries.size', this.chipPinQueries.size);
     console.log('chipQueries.size', this.chipQueries.size);
     console.log('wirePointQueries.size', this.wirePointQueries.size);
@@ -208,11 +252,15 @@ export class SimulationEntityManager<T> {
 
   update() {
     for (const wirePoint of this.wirePointQueries) {
-      wirePoint.isActivated = this.adapter.getPowerState(wirePoint.pinId) === POWER_STATE_HIGH;
+      wirePoint.powerState = this.adapter.getPowerState(wirePoint.pinId);
     }
 
-    for (const io of this.ioQueries) {
-      io.namedPin.powerState = this.adapter.getPowerState(io.namedPin.id);
+    for (const input of this.inputQueries) {
+      input.namedPin.powerState = this.adapter.getPowerState(input.namedPin.id);
+    }
+
+    for (const output of this.outputQueries) {
+      output.namedPin.powerState = this.adapter.getPowerState(output.namedPin.id);
     }
 
     for (const chipPin of this.chipPinQueries) {
