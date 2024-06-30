270021918c98b1a198fbbc000da59bdb5b192f0e
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-06-30 23:21:44 +0300

    created the input and output for drawing on the canvas

---
+++ src/lib/frontend/input.ts
@@ -0,0 +1,24 @@
+import { DIRECTION } from "$lib/helpers/direction";
+import { IO, type IOProps } from "./io";
+import { POWER_STATE_HIGH } from "./state";
+
+export interface InputProps<T> extends Omit<IOProps<T>, 'direction'> {
+}
+
+export class Input<T> extends IO<T> {
+  state: boolean;
+
+  constructor(props: InputProps<T>) {
+    super({ ...props, direction: DIRECTION.RIGHT });
+
+    this.state = props.namedPin.powerState === POWER_STATE_HIGH;
+  }
+
+  toggle() {
+    this.state = !this.state;
+  }
+
+  get activated() {
+    return this.state;
+  }
+}
---
+++ src/lib/frontend/output.ts
@@ -0,0 +1,11 @@
+import { DIRECTION } from "$lib/helpers/direction";
+import { IO, type IOProps } from "./io";
+
+export interface OutputProps<T> extends Omit<IOProps<T>, 'direction'> {
+}
+
+export class Output<T> extends IO<T> {
+  constructor(props: OutputProps<T>) {
+    super({ ...props, direction: DIRECTION.LEFT });
+  }
+}
