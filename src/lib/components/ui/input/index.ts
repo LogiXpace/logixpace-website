a2a640f85ab2f43a9e0226083bc18d94006c3ef4
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-07-01 18:16:51 +0300

    added input and label components

---
+++ src/lib/components/ui/input/index.ts
@@ -0,0 +1,10 @@
+import Root from "./input.svelte";
+
+export * from "./input.svelte";
+
+
+export {
+	Root,
+	//
+	Root as Input,
+};
---
+++ src/lib/components/ui/label/label.svelte
@@ -0,0 +1,28 @@
+<script context="module" lang="ts">
+	import { Label as LabelPrimitive } from 'bits-ui';
+	import type { Snippet } from 'svelte';
+
+	export type LabelProps = LabelPrimitive.Props & {
+		children: Snippet;
+	};
+	export type LabelEvents = LabelPrimitive.Events;
+</script>
+
+<script lang="ts">
+	import { cn } from '$lib/utils/tailwindcss';
+
+	type $$Props = LabelProps;
+	type $$Events = LabelEvents;
+
+	let { class: className, children, ...restProps }: LabelProps = $props();
+</script>
+
+<LabelPrimitive.Root
+	class={cn(
+		'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+		className
+	)}
+	{...restProps}
+>
+	{@render children()}
+</LabelPrimitive.Root>
