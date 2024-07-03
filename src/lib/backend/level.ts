76fbdbb574bcaf9929a46eb59854e984dd8bf737
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-07-03 20:17:10 +0300

    fixed som git stuff

--- src/lib/backend/level.ts
+++
@@ -1,6 +0,0 @@
-export const MAXIMUM_LEVEL = 1000;
-
-/**
- * @typedef Level
- * @type {number}
- */
--- src/lib/backend/power-state.ts
+++
@@ -1,8 +0,0 @@
-/**
- * the power state
- */
-
-export const POWER_STATE_LOW = 0;
-export const POWER_STATE_HIGH = 1;
-
-export type PowerState = number;
